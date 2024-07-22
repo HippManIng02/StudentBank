aa2905021e3bafebeec40652595777638b4f71a6
Yao-Hippolyte FOLLY <yao-hippolyte.folly@ens.uvsq.fr>
2024-07-22 14:27:23 +0200

    Réalisation de la page Home 1

--- lib/views/widgets/gradient.dart
+++ lib/views/widgets/gradient.dart
@@ -1,16 +1,15 @@
 import 'package:flutter/material.dart';
 
-
 class GradientText extends StatelessWidget {
+  final String text;
+  final TextStyle? style;
+  final Gradient gradient;
   const GradientText({
     super.key,
     required this.text,
     this.style,
     required this.gradient,
   });
-  final String text;
-  final TextStyle? style;
-  final Gradient gradient;
   @override
   Widget build(BuildContext context) {
     return ShaderMask(
@@ -19,6 +18,31 @@ class GradientText extends StatelessWidget {
         Rect.fromLTWH(0, 0, bounds.width, bounds.height),
       ),
       child: Text(text, style: style),
+    );
+  }
+}
+
+class GradientColor extends StatelessWidget {
+  final Gradient gradient;
+  final Widget content;
+  final EdgeInsetsDirectional? margin;
+  const GradientColor({
+    super.key,
+    required this.gradient,
+    required this.content,
+    this.margin,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        margin: margin,
+        decoration: BoxDecoration(
+          gradient: gradient,
+          borderRadius: BorderRadius.circular(30.0),
+        ),
+        child: content,
+      ),
     );
   }
 }
